14fb9188e1678c24d33066f1fcf7463417a87495
antonysavache <89783847+antonysavache@users.noreply.github.com>
2025-06-17 13:26:56 +0200

    fixed crypto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as crypto from 'crypto';
 
 // Загружаем переменные окружения
 dotenv.config();
+
+// Исправляем проблему с crypto для @nestjs/schedule
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
